7685724d8c77c5a1852d3d75d9cbd75fe1e6a064
smd <andriipushkar@gmail.com>
2025-10-30 01:56:51 +0200

    Update gitignore, upload script and config file

--- upload_to_github.sh
+++ upload_to_github.sh
@@ -5,7 +5,7 @@ echo "Видалення старого remote origin..."
 git remote remove origin
 
 echo "Додавання нового remote origin..."
-git remote add origin https://github.com/andriipushkar/scalp-v1
+git remote add origin https://github.com/andriipushkar/scalp-v1.2
 
 echo "Перевірка нового remote..."
 git remote -v
